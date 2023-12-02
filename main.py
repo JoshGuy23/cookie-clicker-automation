f7b4e35cc5a1eca0346d3ab42f1c1b17272af20d
Joshua Hecker <jhecker2001@gmail.com>
2023-12-02 13:37:29 -0600

    Added functionality to stop clicking and display the achieved cookies per second after 5 minutes.

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
+def upgrades_available(drv):
+    x_path = '//*[@id="store"]'
+    stores = [drv.find_element(By.XPATH, value=f'{x_path}/div[{i}]') for i in range(1, 10)]
+    for shop in stores:
+        if shop.get_attribute("class") != "grayed":
+            return True
+    return False
+
+
 def buy_upgrade(drv):
     x_path = '//*[@id="store"]'
     stores = [drv.find_element(By.XPATH, value=f'{x_path}/div[{i}]') for i in range(1, 10)]
 
-    affordable = [store for store in stores if store.get_attribute("class") != "grayed"]
+    affordable = [shop for shop in stores if shop.get_attribute("class") != "grayed"]
     prices = [int(i.find_element(By.CSS_SELECTOR, value="b").text.split("-")[1].split()[0]) for i in affordable]
 
     most_expensive = max(prices)
@@ -25,13 +34,17 @@ driver.get("https://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.CSS_SELECTOR, value="#cookie")
 
 five_seconds = time.time() + 5
-five_minutes = time.time() + 15
+five_minutes = time.time() + 60 * 5
 
 while time.time() < five_minutes:
     cookie.click()
 
     if time.time() >= five_seconds:
-        upgrade = buy_upgrade(driver)
-        upgrade.click()
+        if upgrades_available(driver):
+            upgrade = buy_upgrade(driver)
+            upgrade.click()
         five_seconds = time.time() + 5
     pass
+
+cookies_per_second = float(driver.find_element(By.ID, value="cps").text.split(":")[1].split()[0])
+print(f"You have achieved {cookies_per_second} cookies per second!")
